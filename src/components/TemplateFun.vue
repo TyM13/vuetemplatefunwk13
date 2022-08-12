472f19ae5396cc834a177056ae3f6d1814315233
Your Name <you@example.com>
2022-08-11 18:04:01 -0600

    templatefunwk13 knowledge check

---
+++ src/components/TemplateFun.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+<h1>{{blog_title}}</h1>
+<p>{{blog_content}}</p>
+<img :src="blog_image_url" alt="tower of appartments">
+
+  </div>
+</template>
+
+<script>
+  export default {
+    
+data() {
+  return {
+    blog_content: 'blog content',
+    blog_title: 'blog title',
+    blog_image_url: 'https://images.unsplash.com/photo-1660070918769-f34c724cfe43?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=685&q=80'
+  }
+},
+
+  }
+</script>
+
+<style scoped>
+</style>
